84728ec7d9e31193f260c4e755c9dbec83f3e70f
Yoann Chaudet <yoannchaudet@github.com>
2021-04-10 21:57:37 -0700

    Revert "Simplify Get-Lists"
    This reverts commit 3a7da622b6fca3993f983602d1c37e53a4272d70.

--- src/public/Get-Lists.ps1
+++ src/public/Get-Lists.ps1
@@ -10,30 +10,29 @@ function Get-Lists {
   The formatted string.
   #>
 
-  param (
-    [object[]] $Lists
-  )
+  # TODO: Document the prefixes
 
-  # Get prefixes
-  $listFirstPrefix = Get-SettingValue 'LIST_FIRST_PREFIX' '┌─ '
-  $listNPrefix = Get-SettingValue 'LIST_N_PREFIX' '├─ '
-  $listLastPrefix = Get-SettingValue 'LIST_LAST_PREFIX' '└─ '
-  $listOneItemPrefix = Get-SettingValue 'LIST_ONE_ITEM_PREFIX' ' ─ '
+  param (
+    [object[]] $Lists,
+    [String] $ListFirstPrefix = (Get-SettingValue 'LIST_FIRST_PREFIX' '┌─ '),
+    [String] $ListNPrefix = (Get-SettingValue 'LIST_N_PREFIX' '├─ '),
+    [String] $ListLastPrefix = (Get-SettingValue 'LIST_LAST_PREFIX' '└─ '),
+    [String] $ListOneItemPrefix = (Get-SettingValue 'LIST_ONE_ITEM_PREFIX' ' ─ ')
+  )
 
   # Iterate over the lists
   $Lists | Where-Object { $_ } | ForEach-Object {
-
-    # Iterate over the list in the current list
+    # Iterate over the list of list (or singleton)
     $list = @($_)
     for ($i = 0; $i -lt $list.Count; $i++) {
       # Select the appropriate prefix
-      $prefix = $listNPrefix
+      $prefix = $ListNPrefix
       if ($i -eq 0 -and $list.Count -eq 1) {
-        $prefix = $listOneItemPrefix
+        $prefix = $ListOneItemPrefix
       } elseif ($i -eq 0) {
-        $prefix = $listFirstPrefix
+        $prefix = $ListFirstPrefix
       } elseif ($i -eq ($list.Count -1)) {
-        $prefix = $listLastPrefix
+        $prefix = $ListLastPrefix
       }
 
       # Output the element
