52c62229de71ccb3c993849cfb9b21fd6681207e
Yoann Chaudet <yoannchaudet@github.com>
2021-04-22 08:55:48 -0700

    Fix module manifest and add publish task

--- build.ps1
+++ build.ps1
@@ -10,13 +10,16 @@ The task to run.
 
 - Test, run unit tests
 - Import, import the module in the current session (useful during development)
+- Publish, publish the module to the gallery
 
 .PARAMETER Module
 The name of the module for which the script runs.
 #>
 
+[CmdletBinding(SupportsShouldProcess=$True)]
+
 param (
-  [ValidateSet('Test', 'Import')]
+  [ValidateSet('Test', 'Import', 'Publish')]
   [Parameter(Position = 0)]
   [string] $Task = 'Test',
   [string] $Module = 'Hospitable'
@@ -58,5 +61,30 @@ Invoke-Pester -Configuration `$configuration
   'Import' {
     Remove-Module -Name $Module -Force -ErrorAction 'SilentlyContinue'
     Import-Module -Name (Join-Path $PSScriptRoot "src/$($Module).psm1")
+  }
+
+  'Publish' {
+    # Validate a NUGET_API_KEY environment variable was provided
+    if (-Not (Test-Path env:NUGET_API_KEY)) {
+      throw "`$env:NUGET_API_KEY is undefined (required to publish the module)"
+    }
+
+    # Prepare the module to publish (ignore what if preference)
+    $releaseModuleFolder = Join-Path ([System.IO.Path]::GetTempPath()) ([System.Guid]::NewGuid()) $Module
+    New-Item -ItemType Directory -Path $releaseModuleFolder -Force -WhatIf:$false | Out-Null
+    Copy-Item `
+      -Path (Join-Path $PSScriptRoot 'src' '*') `
+      -Destination $releaseModuleFolder `
+      -Exclude '*.Tests.ps1' `
+      -Recurse `
+      -WhatIf:$false
+
+    # Publish the module
+    Publish-Module `
+      -Path $releaseModuleFolder `
+      -Repository PSGallery `
+      -NuGetApiKey $env:NUGET_API_KEY `
+      -Verbose `
+      -WhatIf:$WhatIfPreference
   }
 }
